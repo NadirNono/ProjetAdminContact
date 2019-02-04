b49e81823b9427821e4c74eef31e195ced478fb3
nadir <nadir.elamelsi@gmail.com>
2019-02-04 18:51:11 +0100

    ajout fichier

---
+++ src/Entity/Contact.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ContactRepository")
+ */
+class Contact
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $ipContact;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateContact;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $subject;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $message;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIpContact(): ?string
+    {
+        return $this->ipContact;
+    }
+
+    public function setIpContact(string $ipContact): self
+    {
+        $this->ipContact = $ipContact;
+
+        return $this;
+    }
+
+    public function getDateContact(): ?\DateTimeInterface
+    {
+        return $this->dateContact;
+    }
+
+    public function setDateContact(\DateTimeInterface $dateContact): self
+    {
+        $this->dateContact = $dateContact;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(string $subject): self
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(
+ *     fields={"email"},
+ *     message="L'email existe déjà"
+ * )
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Email()
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min="8", minMessage="Votre mot de passe doit faire minimum 8 caractères")
+     */
+    private $password;
+
+    /**
+     * @Assert\EqualTo(propertyPath="password", message="Vous n'avez pas tapé le même mot de passe !")
+     */
+    public $confirm_password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $roleuser;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $employee;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateInscription;
+
+    public function getId() : ? int
+    {
+        return $this->id;
+    }
+
+    public function getEmail() : ? string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email) : self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getUsername() : ? string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username) : self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword() : ? string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password) : self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getRoleuser() : ? string
+    {
+        return $this->roleuser;
+    }
+
+    public function setRoleuser(string $roleuser) : self
+    {
+        $this->roleuser = $roleuser;
+
+        return $this;
+    }
+
+    public function getEmployee() : ? string
+    {
+        return $this->employee;
+    }
+
+    public function setEmployee(string $employee) : self
+    {
+        $this->employee = $employee;
+
+        return $this;
+    }
+
+    public function getDateInscription() : ? \DateTimeInterface
+    {
+        return $this->dateInscription;
+    }
+
+    public function setDateInscription(\DateTimeInterface $dateInscription) : self
+    {
+        $this->dateInscription = $dateInscription;
+
+        return $this;
+    }
+
+    public function getRoles(){
+        return ['ROLE_USER'];
+    }
+
+        public function getSalt(){
+    }
+
+        public function eraseCredentials(){
+    }
+}
